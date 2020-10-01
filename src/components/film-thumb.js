a4cd75dfed7ba538a20f15405d64705eec68a7ff
Vlad Nosenko <vladnosenko96@gmail.com>
2020-10-01 13:36:42 +0300

    main page sections

--- src/components/film-thumb.js
+++ src/components/film-thumb.js
@@ -1,21 +1,22 @@
-import React, {useEffect, useRef} from "react";
+import React, {useRef} from "react";
 import img from "../images/filmThumb.png";
 import {gsap} from "gsap";
 
-const FilmThumb = (props) => {
+const FilmThumb = ({film}) => {
     let topContent = useRef(null);
     let botContent = useRef(null);
 
+
     const mouseOver = () => {
         gsap.to([topContent], 0, {
-            duration: .8,
+            duration: 1,
             ease: "power3.In",
             y: 50,
             opacity: 1,
         });
 
         gsap.to([botContent], 0, {
-            duration: .8,
+            duration: 1,
             ease: "power3.In",
             y: -65,
             opacity: 1,
@@ -27,62 +28,61 @@ const FilmThumb = (props) => {
             y: -50,
             ease: "power3.Out",
             opacity: 0,
+            delay: .1
         });
         gsap.to([botContent], 0, {
             y: 0,
             ease: "power3.Out",
+            delay: .1
         });
     };
 
-    const filmProps =
-        {
-            genre: "драма",
-            name: "Брати. Остання сповідь ",
-            year: "2020",
-            duration: "123 хв",
-            director: "Вікторія Трохименко",
-            roles: "Ґольшіфтег Фарагані, Мадж Мастура, Айша Бен Мілед",
-            about: "Історія любові та ненависті двох братів, які доживають віку в карпатському селі. Історія любові та ненависті двох братів, які доживають віку в карпатському селі.",
-            image: ""
-        }
-    ;
-
-
     return (
-        <div className="film-thumb" onMouseOver={mouseOver} onMouseLeave={mouseLeave}>
-            <img className="film-image" src={img} alt="film"/>
-            <div className="film-info">
-                <div
-                    ref={el => (topContent = el)}
-                    className="film-info-block film-info-block__top"
-                >
-                    <div className="genre">{filmProps.genre}</div>
-                    <div className="credits">
-                        <div className="credit">
-                            <h4 className="credit-title">Режисер </h4>
-                            <span> - </span>
-                            <span className="credit-text">{filmProps.director}</span>
+        <div className={`film-thumb ${film.archive ? "archive" : null}`}
+             onMouseOver={mouseOver}
+             onMouseLeave={mouseLeave}
+        >
+            <img className="film-image" src={film.image} alt="film"/>
+            {
+                !film.archive
+                    ? (
+                        <div className="film-info">
+                            <div
+                                ref={el => (topContent = el)}
+                                className="film-info-block film-info-block__top"
+                            >
+                                <div className="genre">{film.genre}</div>
+                                <div className="credits">
+                                    <div className="credit">
+                                        <h4 className="credit-title">Режисер</h4>
+                                        <span> - </span>
+                                        <span className="credit-text">{film.director}</span>
+                                    </div>
+                                    <div className="credit">
+                                        <h4 className="credit-title">У ролях</h4>
+                                        <span> - </span>
+                                        <span className="credit-text">{film.roles}</span>
+                                    </div>
+                                </div>
+                            </div>
+                            <div
+                                ref={el => (botContent = el)}
+                                className="film-info-block film-info-block__bottom"
+                            >
+                                <div className="numbers">
+                                    <div className="number">{film.year}</div>
+                                    <div className="number">{film.duration}</div>
+                                </div>
+                                <h2 className="name">{film.name}</h2>
+                                <div className="about">{film.about}</div>
+                            </div>
                         </div>
-                        <div className="credit">
-                            <h4 className="credit-title">У ролях</h4>
-                            <span> - </span>
-                            <span className="credit-text">{filmProps.roles}
-                        </span>
-                        </div>
+                    )
+                    : <div className="film-info-archive">
+                        <div className="film-year">{film.year}</div>
                     </div>
-                </div>
-                <div
-                    ref={el => (botContent = el)}
-                    className="film-info-block film-info-block__bottom"
-                >
-                    <div className="numbers">
-                        <div className="number">{filmProps.year}</div>
-                        <div className="number">{filmProps.duration}</div>
-                    </div>
-                    <h2 className="name">{filmProps.name}</h2>
-                    <div className="about">{filmProps.about}</div>
-                </div>
-            </div>
+            }
+
         </div>
     )
 };
